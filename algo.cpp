67572d154cfb61222c4530fb6673c412cd5b7386
dinku mehta <mehtadinku@gmail.com>
2021-01-20 18:24:33 +0530

    first commit

---
+++ algo.cpp
@@ -0,0 +1,16 @@
+
+// Sieve of Eratosthenes
+
+int n;
+vector<char> is_prime(n+1, true);
+is_prime[0] = is_prime[1] = false;
+for (int i = 2; i <= n; i++) {
+    if (is_prime[i] && (long long)i * i <= n) {
+        for (int j = i * i; j <= n; j += i)
+            is_prime[j] = false;
+    }
+}
+
+
+---------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<char> vc;
+typedef vector<string> vs;
+
+#define PB push_back
+#define MP make_pair
+
+#define FOR(i,a,b)  for(int i=a;i<b;i++)
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+#endif
+
+ll t; cin >> t;
+while(t--) {
+
+}
+
+
+
+}
+
+
+
